fd59f8cdb174c4de51f5964a0f1131d8c10bf10f
chester425 <chester425@mail.ru>
2021-06-22 14:08:55 +0300

    fixed the task read more

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const getRandomFloat = (min, max, digits) => {
     return;
   }
   max = Math.floor(max);
-  return Math.random().toFixed(digits) * (max - min ) + min;
+  return (Math.random() * (max - min ) + min).toFixed(digits);
 };
 
 getRandomFloat(0, 100, 2);
@@ -22,33 +22,33 @@ getRandomFloat(0, 100, 2);
 
 const NUMBER_ADS = 10;
 const TYPE_OF_HOUSE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECKIN = ['12:00', '13:00', '14:00'];
-const CHECKOUT = ['12:00', '13:00', '14:00'];
+const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 const OPTIONS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 
-const LOCATION_LAT = {
+const LocationLat = {
   MIN: 35.65000,
   MAX:35.70000,
 };
 
-const LOCATION_LNG = {
+const LocationLng = {
   MIN: 139.70000,
   MAX:139.80000,
 };
 
-const PRICE = {
+const Price = {
   MIN: 1000,
   MAX: 10000,
 };
 
-const ROOMS = {
+const Rooms = {
   MIN: 1,
   MAX: 5,
 };
 
-const GUESTS = {
+const Guests = {
   MIN: 1,
   MAX: 6,
 };
@@ -58,7 +58,10 @@ const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.le
 const getAds = () => {
   const adsArray = [];
   for (let i = 1; i <= NUMBER_ADS; i++) {
-    i < 10? i = `0${  i}` : i;
+    const LAT = getRandomFloat(LocationLat.MIN, LocationLat.MAX, 5);
+    const LNG = getRandomFloat(LocationLng.MIN, LocationLng.MAX, 5);
+    i = i.toString().padStart(2, '0');
+
     const ad = {
       author: {
         avatar: `img/avatars/user${  i  }.png`, // адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
@@ -66,21 +69,21 @@ const getAds = () => {
 
       offer: {  //объект — содержит информацию об объявлении. Состоит из полей:
         title: 'Крутая хата', //строка — заголовок предложения. Придумайте самостоятельно.
-        address: `${getRandomFloat(LOCATION_LAT.MIN, LOCATION_LAT.MAX).toString()  }, ${  getRandomFloat(LOCATION_LNG.MIN, LOCATION_LNG.MAX).toString()}`, //строка — адрес предложения. Для простоты пусть пока  составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
-        price: getRandomInt(PRICE.MIN,PRICE.MAX), //число — стоимость. Случайное целое положительное число.
+        address: `${LAT  }, ${  LNG}` , //строка — адрес предложения. Для простоты пусть пока  составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
+        price: getRandomInt(Price.MIN,Price.MAX), //число — стоимость. Случайное целое положительное число.
         type: getRandomArrayElement(TYPE_OF_HOUSE), //строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-        rooms: getRandomInt(ROOMS.MIN,ROOMS.MAX), //число — количество комнат. Случайное целое положительное число.
-        guests: getRandomInt(GUESTS.MIN,GUESTS.MAX), //число — количество гостей, которое можно разместить. Случайное целое положительное число.
-        checkin: getRandomArrayElement(CHECKIN), //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-        checkout: getRandomArrayElement(CHECKOUT), //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+        Rooms: getRandomInt(Rooms.MIN,Rooms.MAX), //число — количество комнат. Случайное целое положительное число.
+        guests: getRandomInt(Guests.MIN,Guests.MAX), //число — количество гостей, которое можно разместить. Случайное целое положительное число.
+        checkin: getRandomArrayElement(CHECKIN_TIMES), //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+        checkout: getRandomArrayElement(CHECKOUT_TIMES), //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
         features: OPTIONS.slice(getRandomInt (0, OPTIONS.length)) , //массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
         description: 'Великолепная квартира полностью укомплектована и недавно отремонтирована.', //строка — описание помещения. Придумайте самостоятельно.
         photos: PHOTOS.slice(getRandomInt (0, PHOTOS.length)), //массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
       },
 
       location: {
-        lat: getRandomFloat(LOCATION_LAT.MIN, LOCATION_LAT.MAX), // координата x широта.
-        lng: getRandomFloat(LOCATION_LNG.MIN, LOCATION_LNG.MAX), // координата y метки на карте .
+        lat: Number(LAT), // координата x широта.
+        lng: Number(LNG), // координата y метки на карте .
       },
 
     };
